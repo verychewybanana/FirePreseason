aa3281e718af7bcdcce8c6ebed4e8601fdb61a25
papereater42 <chemist753@gmail.com>
2024-02-05 18:32:47 -0500

    readjusted odowheel coords

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -43,8 +43,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
 
     public StandardTrackingWheelLocalizer(HardwareMap hardwareMap, List<Integer> lastTrackingEncPositions, List<Integer> lastTrackingEncVels) {
         super(Arrays.asList(
-                new Pose2d(-2.8, LATERAL_DISTANCE / 2, 0), // left
-                new Pose2d(-2.8, -LATERAL_DISTANCE / 2, 0), // right
+                new Pose2d(-2.625, LATERAL_DISTANCE / 2, 0), // left
+                new Pose2d(-2.5, -LATERAL_DISTANCE / 2, 0), // right
                 new Pose2d(FORWARD_OFFSET, 2.8, Math.toRadians(90)) // front
         ));
 
